4330fe00813ff84f29e0d13303e6adaf590263bb
caolv <shaoyinshaoyang@gmail.com>
2024-10-06 22:26:19 +0800

    14.1.1 创建抽象IRequest层

---
+++ ziface/irequest.go
@@ -0,0 +1,12 @@
+// Package ziface zinx/znet/irequest.go
+package ziface
+
+/*
+	IRequest 接口：
+	实际上是把客户端请求的连接信息和请求的数据包装到Request里
+*/
+
+type IRequest interface {
+	GetConnection() IConnection // 获取请求连接信息
+	GetData() []byte            // 获取请求消息的数据
+}
