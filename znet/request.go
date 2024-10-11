06755b94370742747b2c0c850ecacc5a0613da9f
caolv <shaoyinshaoyang@gmail.com>
2024-10-11 22:49:56 +0800

    16.3.1 Request字段修改

--- znet/request.go
+++ znet/request.go
@@ -5,15 +5,20 @@ import "zinx/ziface"
 
 type Request struct {
 	conn ziface.IConnection // 已经和客户端建立好连接
-	data []byte             // 客户端请求的数据
+	msg  ziface.IMessage    // 客户端请求的数据
 }
 
-// 获取请求连接信息
+// GetConnection 获取请求连接信息
 func (r *Request) GetConnection() ziface.IConnection {
 	return r.conn
 }
 
-// 获取请求消息的数据
+// GetData 获取请求消息的数据
 func (r *Request) GetData() []byte {
-	return r.data
+	return r.msg.GetData()
+}
+
+// GetMsgId 获取请求的消息的 ID
+func (r *Request) GetMsgId() uint32 {
+	return r.msg.GetMsgId()
 }
